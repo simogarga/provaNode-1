3b5bb72ef758771ca0df8ad49a3d24dfbb342e00
Gargano Simone <gargano.simone@osdbsesto>
2021-01-28 13:31:51 +0100

    28-01

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
 var http = require("http");
 var express = require("express");
+var apiServer = express();
 
 var hostname = "127.0.0.1";
 var port = 3000;
+var nome = "nodegargano";
+var path = require("path");
 
-var server = http.createServer((req, res) => {
-    console.log("req:", req);
-    res.statusCode = 200;
-    res.setHeader("content-Type", "text/plain");
-    res.end("Ciao belloccio!");
-});
-
-server.listen(port, hostname, () => {
+apiServer.listen(port, hostname, () =>{
     console.log("server running at http://%s:%d/", hostname, port);
 });
+    
+
+
+apiServer.get("/nome", (req, res) => {
+    console.log("cc");
+    res.send("Ciao!"+nome);
+});
+
+apiServer.get("/", function(req,res){
+    res.send("sei in home");
+});
+
+apiServer.get("/index", (req,res) => {
+  //  res.sendFile("C:\\Users\\gargano.simone\\Documents\\provaNode-1\\index.html");
+  res.sendFile(path.join(__dirname + "/index.html"));
+})
